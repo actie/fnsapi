7e24c0654a2032f9a6438e76ac0a438d5cd38c34
Fedor Koshel <fedorkk@gmail.com>
2019-04-20 16:41:15 +0200

    get mesage service

--- spec/get_message_service_spec.rb
+++ spec/get_message_service_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Fnsapi::GetMessageService do
     allow_any_instance_of(Fnsapi::Configuration).to receive(:fnsapi_master_key).and_return('test_key')
     allow(Fnsapi::TmpStorage).to receive(:new).and_return(StubbedTmpStorage.new)
     allow_any_instance_of(Fnsapi::AuthService).to receive(:reset_credentials).and_return('token')
-    allow_any_instance_of(StubbedTmpStorage).to receive(:token).and_return('token')
   end
 
   let(:instance) { described_class.new }
@@ -45,45 +44,58 @@ RSpec.describe Fnsapi::GetMessageService do
 
     subject(:call) { instance.call(message_id) }
 
-    context 'when user is not specified' do
-      it 'initializes client with auth_params' do
-        expect(Savon).to receive(:client).with(
-          hash_including(
-            headers: {
-              'FNS-OpenApi-Token' => 'token',
-              'FNS-OpenApi-UserToken' => Base64.strict_encode64('default_user'.to_s)
-            }
-          )
-        ).and_call_original
+    context 'when token is not defined' do
+      it 'calls reset_credentials from AuthService' do
+        expect_any_instance_of(Fnsapi::AuthService).to receive(:reset_credentials) { 'some token' }
         call
       end
     end
 
-    context 'when user is specified' do
-      subject(:call) { instance.call(message_id, 123) }
+    context 'when token is defined' do
+      before do
+        allow_any_instance_of(StubbedTmpStorage).to receive(:token) { 'token' }
+      end
 
-      it 'initializes client with auth_params' do
-        expect(Savon).to receive(:client).with(
-          hash_including(
-            headers: {
-              'FNS-OpenApi-Token' => 'token',
-              'FNS-OpenApi-UserToken' => Base64.strict_encode64(123.to_s)
-            }
-          )
-        ).and_call_original
+      context 'when user is not specified' do
+        it 'initializes client with auth_params' do
+          expect(Savon).to receive(:client).with(
+            hash_including(
+              headers: {
+                'FNS-OpenApi-Token' => 'token',
+                'FNS-OpenApi-UserToken' => Base64.strict_encode64('default_user'.to_s)
+              }
+            )
+          ).and_call_original
+          call
+        end
+      end
+
+      context 'when user is specified' do
+        subject(:call) { instance.call(message_id, 123) }
+
+        it 'initializes client with auth_params' do
+          expect(Savon).to receive(:client).with(
+            hash_including(
+              headers: {
+                'FNS-OpenApi-Token' => 'token',
+                'FNS-OpenApi-UserToken' => Base64.strict_encode64(123.to_s)
+              }
+            )
+          ).and_call_original
+          call
+        end
+      end
+
+      it 'calls :get_message with correct parameters' do
+        expect_any_instance_of(Savon::Client).to(
+          receive(:call).with(:get_message, message: correct_message_hash).and_return(stubbed_response)
+        )
         call
       end
-    end
 
-    it 'calls :get_message with correct parameters' do
-      expect_any_instance_of(Savon::Client).to(
-        receive(:call).with(:get_message, message: correct_message_hash).and_return(stubbed_response)
-      )
-      call
-    end
-
-    it 'returns parsed response' do
-      expect(call).to eq('test')
+      it 'returns parsed response' do
+        expect(call).to eq('test')
+      end
     end
   end
 end
